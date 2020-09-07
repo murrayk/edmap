1b35f751c9caaa5d97df0ae678ff16c69a19d843
murray <murrayhking@gmail.com>
2020-09-07 21:07:28 +0100

    Test extrusion

--- src/App.js
+++ src/App.js
@@ -29,28 +29,30 @@ const polygonData = [
       [-95.52274057225983, 30.131426214982195],
       [-91.72307036099997, 31.814196736000035]
     ],
-    name: "firstPolygon"
+    name: "firstPolygon",
+    elevation: 10000
   }
 ];
 
-function App({data}) {
+function App() {
 
   const LAYER_POLY = new PolygonLayer({
     id: "poly-layers",
     data: polygonData,
     stroked: true,
     filled: true,
-    extruded: false,
+    extruded: true,
     wireframe: true,
     lineWidthMinPixels: 1,
     getPolygon: d => d.contours,
     getLineColor: [80, 80, 80],
     getFillColor: [80, 80, 80],
+    getElevation: d => d.elevation,
     getLineWidth: 250
   });
 
   const layers = [
-    new LineLayer({id: 'line-layer', data}),
+    new LineLayer({id: 'line-layer', data: data}),
     LAYER_POLY
   ];
 
