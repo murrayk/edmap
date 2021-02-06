3bd851111c907a94c61d03b85f0f67eb9f12d4ed
Murray King <murray.king@ros.gov.uk>
2021-02-06 18:38:01 +0000

    cors enabled S3 end point for prod

---
+++ docs/precache-manifest.7f6a84b0175fed0c05dd7ed3066df94b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1ab6add0e4a22e2d3d089926cf26c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8cd59d11b5af436ddc92",
+    "url": "./static/css/main.a82b812e.chunk.css"
+  },
+  {
+    "revision": "a39e345e9f34ba8d23b1",
+    "url": "./static/js/2.ab478177.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "./static/js/2.ab478177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd59d11b5af436ddc92",
+    "url": "./static/js/main.0d3b4f6b.chunk.js"
+  },
+  {
+    "revision": "23ff3a3940a6c644d8da",
+    "url": "./static/js/runtime-main.13e5b76c.js"
+  }
+]);
